aa6be876b2390d2f9f4ee795e3dac0792faeeac1
Douglas Mendes <douglas.mendes.ia@outlook.com>
2023-03-09 00:04:13 -0300

    individualização

--- src/config_env.py
+++ src/config_env.py
@@ -4,8 +4,8 @@ path_raw_data = '/dbfs/FileStore/E_Commerce_Dataset.xlsx'
 # COMMAND ----------
 
 # Database Feature Store and Feature Table
-feature_store_db_name = 'fs_ecommerce'
-feature_store_db_name_and_table = 'fs_ecommerce.churn'
+feature_store_db_name = 'fs_ecommerce_<MEU-EMAIL>'
+feature_store_db_name_and_table = 'fs_ecommerce_<MEU-EMAIL>.churn'
 
 # COMMAND ----------
 
